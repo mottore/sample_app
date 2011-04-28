53f587b6024d04233738dfcfef91bf0c91802805
Dudu <mottore@gmail.com>
2011-04-28 16:41:21 +0200

    Finished layout and routes

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,8 +1,19 @@
 class PageController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
   end
+  
+  def about
+    @title = "About"
+  end
+
+ def help
+    @title = "Help"
+  end
+
 
 end
--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe PageController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => " | Home")
+    end   
   end
 
   describe "GET 'contact'" do
@@ -14,6 +22,41 @@ describe PageController do
       get 'contact'
       response.should be_success
     end
+    
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => " | Contact")
+    end   
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => " | About")
+    end
+  end
+
+describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content => " | Help")
+    end   
+  end
+
+  
 end
